3a96ec7e83f4c1aa4ad1ea9c4e29dc87fedb54da
wormi <andreas.worm@headissue.com>
2019-06-20 20:55:14 +0200

    try using the selenium server

---
+++ src/test/java/com/headissue/fate/e2e/MyRemoteChromeDriver.java
@@ -0,0 +1,37 @@
+package com.headissue.fate.e2e;
+
+import net.thucydides.core.webdriver.DriverSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+
+public class MyRemoteChromeDriver implements DriverSource {
+
+  @Override
+  public WebDriver newDriver() {
+
+    DesiredCapabilities caps = DesiredCapabilities.chrome();
+    LoggingPreferences logPrefs = new LoggingPreferences();
+    logPrefs.enable(LogType.BROWSER, Level.ALL);
+    caps.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+
+    try {
+      return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public boolean takesScreenshots() {
+    return true;
+  }
+}
